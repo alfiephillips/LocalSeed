040687df130002a8e7457ce542d886bf321ed955
Alfie <thealfiephillips@gmail.com>
2024-03-21 13:16:16 +0000

    Add files via upload

---
+++ LocalSeed/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LocalSeed")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Truro and Penwith College")>
+<Assembly: AssemblyProduct("LocalSeed")>
+<Assembly: AssemblyCopyright("Copyright © Truro and Penwith College 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1fbc56a1-67cb-4caf-b832-e627c02bc7f6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
